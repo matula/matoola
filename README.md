26a1a8886b2e0363995c89f8c4746745e07b9590
Matula <terrymatula@gmail.com>
2013-12-12 11:28:21 -0600

    Readme update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 MatOOla: a simple OO-ish PHP framework/router
 =============================================
+
+## Usage
+
+Just add public methods like `mymethod` to the Controller class, and your url will be `http://server.dev/mymethod`
--- matoola.php
+++ matoola.php
@@ -1,23 +1,40 @@
 <?php
+/**
+ * MatOOla:  a simple OO-ish PHP framework
+ * hat-tip: http://twitto.org/ and https://news.ycombinator.com/item?id=505899
+ *
+ * @author  Terry Matula <terrymatula@gmail.com>
+ *
+ */
+
+// Get controller
 $controller = new ReflectionClass('Controller');
+
 try {
+	// Default to index
 	$method = $controller->getMethod(($_GET['m']) ?: 'index');
 }
 catch(ReflectionException $e)
 {
+	// Method/page not found
 	header('HTTP/1.0 404 Not Found');
 	die('404 error. Page not found.');
 }
 
+// Either no method, or it wasn't public
 if (!$method OR !$method->isPublic())
 {
 	header('HTTP/1.0 404 Bad Request');
 	die('404 error. Page not found.');
 }
 
+// Call the method
 $invoke = $controller->newInstance();
 $method->invoke($invoke);
 
+/**
+ * Controller
+ */
 class Controller
 {
 	public function index()
